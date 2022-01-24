8149c60df4c8053a328c86752ef715b77b3300c0
Женя Вито z z z <evgenizef.ai@gmail.com>
2022-01-25 01:07:48 +1000

    Релизовал простенький магазин

--- ForestConsol/Program.cs
+++ ForestConsol/Program.cs
@@ -13,44 +13,63 @@ namespace ForestConsol
             Console.WriteLine($"Добро пожаловать в систему {Name} ");
 
             Console.WriteLine("Вы назначены на должность рубки леса.");
-            Console.WriteLine("Для рубки дерева используйте команду '/has' ");
-            Console.WriteLine("Для того чтоб войти в магазин введите /shop");
-
+            
             int wood = 10000;// Колличесто дерева.
             int axe = 1;// Уровень топора.
 
-            int priceAxeSharpening = 100;
+            int priceAxeSharp = 100; // Цена заточки топора.
+            int priceAxeCover = 1000; // Цена нового покрытия топора.
+            int priceAxeGrip = 10000; // Цена новой рукоятки топора.
 
+            var axeShop = new Shop();
             while (true)
             {
+                Console.WriteLine("Для рубки дерева используйте команду '/has' ");
+                Console.WriteLine("Для того чтоб войти в магазин введите /shop");
                 Console.WriteLine("Ожидается команда...");
                 string EnterCommand = Console.ReadLine();
                 if (EnterCommand == "/has")
                 {
+                    Console.Clear();
                     wood += axe;
-                    Console.WriteLine("Вы получили 1 дерево");
+                    Console.WriteLine($"Вы получили {axe} дерево");
                 }
                 else if (EnterCommand == "/shop")
                 {
-                    Console.WriteLine($"чтоб заточить топор введите '/sharp'. Стоимость - {priceAxeSharpening}.");
+                    Console.Clear();
+
+                    Console.WriteLine("Добро пожаловать в магазин.");
+
+                    Console.WriteLine($"чтоб заточить топор введите '/sharp'. Стоимость - {priceAxeSharp}.");
+                    Console.WriteLine($"Для новго покрытия топора введите '/cover'. Стоимость - {priceAxeCover}");
+                    Console.WriteLine($"Для новой рукоятки введите '/grip'. Стоимость - {priceAxeGrip}");
 
                     string enterShop = Console.ReadLine();
 
-                    if (enterShop == "/sharp") // Тут ошибка(((
+                    if (enterShop == "/sharp")
                     {
-                        if (wood >= priceAxeSharpening)
-                        {
-                            axe++;
-                            wood += priceAxeSharpening;
-                            Console.WriteLine("Заточка произведена!");
-                        }
+                        axe = axeShop.Buy(ref priceAxeSharp, ref wood, axe, 1);
                     }
-                    
+                    else if (enterShop == "/cover")
+                    {
+                        axe = axeShop.Buy(ref priceAxeCover, ref wood, axe, 5);
+                    }
+                    else if (enterShop == "/grip")
+                    {
+                        axe = axeShop.Buy(ref priceAxeGrip, ref wood, axe, 15);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Произошла ошибка.");
+                    }
+
+                    Console.Clear();
                 }
                 else
                 {
                     Console.WriteLine("Введена не верная команда...");
                 }
+
                 Console.WriteLine($"Колличество дерева : {wood}");
             }
         }
---
+++ ForestConsol/Shop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestConsol
+{
+    public class Shop
+    {
+        public int price { get; set; }
+        public int wood { get; set; }
+
+        public int Buy(ref int price, ref int wood, int axe, int add)
+        {
+            if (wood >= price)
+            {
+                wood -= price;
+
+                price += (price % 2) + 5;
+
+                Console.WriteLine("Успех!");
+
+                return axe + add;
+            }
+            else
+            {
+                Console.WriteLine("Неудача(");
+
+                return axe;
+            }
+        }
+
+    }
+}
